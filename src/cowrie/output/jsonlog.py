8f47a64ab7a3d02b3d7e651702761701340cdfcc
grispan56 <39238107+grispan56@users.noreply.github.com>
2022-12-06 15:52:05 +0200

    Update jsonlog.py

--- src/cowrie/output/jsonlog.py
+++ src/cowrie/output/jsonlog.py
@@ -35,6 +35,17 @@ import cowrie.core.output
 import cowrie.python.logfile
 from cowrie.core.config import CowrieConfig
 
+import random
+import uuid
+import json
+import boto3
+import socket, struct
+from p0f import P0f, P0fException
+import platform
+
+from pathlib import Path
+
+
 
 class Output(cowrie.core.output.Output):
     """
@@ -51,6 +62,12 @@ class Output(cowrie.core.output.Output):
         self.outfile = cowrie.python.logfile.CowrieDailyLogFile(
             base, dirs, defaultMode=0o664
         )
+                
+        my_client_id = str(uuid.uuid4())
+        client = boto3.client('sqs', region_name='us-east-1')
+        sqs = boto3.resource('sqs')
+        self.queue = sqs.get_queue_by_name(QueueName='ssh1')
+
 
     def stop(self):
         self.outfile.flush()
@@ -66,5 +83,80 @@ class Output(cowrie.core.output.Output):
             json.dump(logentry, self.outfile, separators=(",", ":"))
             self.outfile.write("\n")
             self.outfile.flush()
+            sensor_ip=Path('/tmp/ip').read_text()
+            sensor_ip = sensor_ip.strip()
+            cid=Path('/tmp/cid').read_text()
+            cid = cid.strip()
+            segment = Path('/tmp/seg').read_text()
+            segment = segment.strip()
+            sensor_type = Path('/tmp/seg').read_text()
+            sensor_type = sensor_type.strip()
+
+            logentry['sensor_ip']=sensor_ip
+            logentry['cid']=cid
+            logentry['segment']=segment
+            logentry['sensor_type']=sensor_type
+
+            print("---------------------1----------")
+            print(logentry)
+            params = json.dumps(logentry)
+            #normalize data by removing cowrie from entry
+            print("---------------------2----------")
+            logentry['eventid']=logentry['eventid'].replace("cowrie.","")
+            print("---------------------3----------")
+            print(logentry)
+            #add sec ver
+            today = random.randint(11000, 11300)
+            cloudy_day = "raining-%s" % today
+            logentry['weather']=cloudy_day
+            print("---------------------4----------")
+
+            #add sensor os data
+            #dist = platform.dist()
+            print("---------------------15---------")
+
+            logentry['sen_os'] = 'linux'
+            logentry['sen_dist'] = platform.platform()
+            logentry['sen_ver'] = '3'
+            print("---------------------6----------")
+            data = None
+            e = None
+            p0f = P0f("/var/run/p0f.sock") # point this to socket defined with "-s" argument.
+            print("---------------------7----------")
+            try:
+                print("---------------------8----------")
+                data = p0f.get_info(logentry['src_ip'])
+                print("---------------------9----------")
+            except Exception as e:
+    # Invalid query was sent to p0f. Maybe the API has changed?
+                print (e)
+            except (KeyError, e):
+    # No data is available for this IP address.
+                print (e)
+            except (ValueError, e):
+    # p0f returned invalid constant values. Maybe the API has changed?
+                print (e)
+            #print (data)
+            if data:
+                #if data exists about the os , add to dict
+                 print ("777")
+                 logentry['src_uptime_sec']=data['uptime_sec']
+                 logentry['src_os_name']=data['os_name'].decode('utf-8')
+                 print (logentry['src_os_name'])
+                 logentry['src_os_flavor']=data['os_flavor'].decode('ascii')
+                 print (logentry['src_os_flavor'])
+                 logentry['src_language']=data['language'].decode('ascii')
+                 print("---------------------10----------")
+
+#            self._log_writer_queue.put_nowait(json.dumps(logentry))
+            print("---------------------11:wq----------")
+            print (logentry)
+            params = json.dumps(logentry)
+            print (params)
+            print("---------------------12:wq----------")
+            response = self.queue.send_message(MessageBody=params)
+            print("--------------------13----------")
+            print (response)
+
         except TypeError:
             print("jsonlog: Can't serialize: '" + repr(logentry) + "'")
